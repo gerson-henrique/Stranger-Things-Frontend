c1c8e1dbda4b34195882cea5193d6661c4582c91
Gero <gersonhoa@gmail.com>
2022-06-03 23:31:33 -0300

    feat : api changes

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const DEFAULT_TIMEOUT = 30000;
 class CharactersService {
-  constructor({ url = process.env.REACT_APP_HAWKINS_URL, timeout = DEFAULT_TIMEOUT }) {
+  constructor({ url = process.env.REACT_APP_HAWKINS_URL || REACT_APP_UPSIDEDOWN_URL,
+    timeout = DEFAULT_TIMEOUT }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
