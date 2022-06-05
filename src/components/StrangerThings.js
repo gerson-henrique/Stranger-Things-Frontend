95b7bf36e125620426c59147936767598d0a5c7d
Gero <gersonhoa@gmail.com>
2022-06-05 12:56:15 -0300

    test : change a test

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -115,7 +115,7 @@ class StrangerThings extends React.Component {
           hereIsTheUpsideDownWorld,
         )}` }
       >
-        {process.env.REACT_APP_DEVELOPMENT ? (<div> Em desenvolvimento </div>) : null}
+        {process.env.REACT_APP_DEVELOPMENT ? (<div>Em desenvolvimento</div>) : null}
         <div className="content strangerfy">
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
